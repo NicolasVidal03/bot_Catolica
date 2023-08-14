72ed9cdda86db189254d546ea8857fbeff5ecd8f
Nicolas <nicolas.vidal@ucb.edu.bo>
2023-08-14 14:36:07 -0400

    Bot saluda por nombre

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './App';
+
+const nombre = document.querySelector("#input-nombre");
+const form = document.querySelector("#form-saludo");
+const saludo = document.querySelector("#saludo");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    saludo.innerHTML = "<p>Hola " + nombre.value + ", como estás?</p>";
+    
+});
