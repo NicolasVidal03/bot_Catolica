4afd3f5ea687f368797adc93ab2f49aa108f871d
Nicolas <nicolas.vidal@ucb.edu.bo>
2023-08-14 15:08:40 -0400

    Bot saluda dependiendo del genero de la persona

--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ const nombre = document.querySelector("#input-nombre");
 const edad = document.querySelector("#input-edad");
 const form = document.querySelector("#form-saludo");
 const saludo = document.querySelector("#saludo");
+const genero = document.querySelector("#select-genero");
 
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     let x = "";
+
     if(edad.value >= 30){
         x = "Sr. ";
+        if(genero.value == "F"){
+            x = "Sra. ";
+        }
     }
-    saludo.innerHTML = "<p>Hola " + x + nombre.value + ", como estás?</p>";
+    else{
+        x = "joven ";
+        if(genero.value == "F"){
+            x = "Srta. ";
+        }
+    }
 
+    saludo.innerHTML = "<p>Hola " + x + nombre.value + ", como está?</p>";
 });
