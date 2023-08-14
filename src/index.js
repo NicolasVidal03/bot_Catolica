ca3d4db5653ecc7d7cee5af7e5a8ca87f403bf46
Nicolas <nicolas.vidal@ucb.edu.bo>
2023-08-14 14:53:36 -0400

    El bot saluda dependiendo de la edad

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import './App';
 
 const nombre = document.querySelector("#input-nombre");
+const edad = document.querySelector("#input-edad");
 const form = document.querySelector("#form-saludo");
 const saludo = document.querySelector("#saludo");
 
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    let x = "";
+    if(edad.value >= 30){
+        x = "Sr. ";
+    }
+    saludo.innerHTML = "<p>Hola " + x + nombre.value + ", como estás?</p>";
 
-    saludo.innerHTML = "<p>Hola " + nombre.value + ", como estás?</p>";
-    
 });
